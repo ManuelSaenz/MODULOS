2030c0386e6b19eff96ee1c484f10de58afb81ba
Manuel Saenz <manuel.saenz11@hotmail.com>
2022-10-11 20:13:55 -0700

    Se creo base para el tema de Modulos (main.py y zen372.py)

---
+++ zen372.py
@@ -0,0 +1,17 @@
+import this
+
+# Comentario en una linea
+"""
+Comentario en multiple lineas
+linea 1
+linea 2
+linea 3
+...
+"""
+#Zen de la clase 372
+print("Sarahi: No se")
+print("Dulce: Mas vale pedir perdon que pedir permiso")
+print("Marco Sebastian: Camaraon que se duerme se lo lleva la bla bla bla")
+print("Fuga x 2")
+
+barba = "Algo, que sorpresa!!!"
